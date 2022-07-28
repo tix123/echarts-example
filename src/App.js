134b5111e492ac60b1ff7a7270421b54b9f60763
Sean Chen <tix123@msn.com>
2022-07-28 00:34:09 -0600

    Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import * as echarts from 'echarts';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  useEffect(() => {
+
+    //Copy from the official line chart example 
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+
+    window.addEventListener('resize', myChart.resize); //For the Resize function
+
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="main" style={{minHeight:600}}></div>
   );
 }
 
